bfa2f0ad23bfeda40e93d2a6814a78c35fc79b53
Michael <michael.ndukwe@hackney.gov.uk>
2021-01-07 17:13:53 +0000

    added image

--- app.rb
+++ app.rb
@@ -1,5 +1,17 @@
 require 'sinatra'
 
+set :session_secret, 'super secret'
+
 get '/' do
   'hello!'
+end
+
+get '/secret' do
+  "this is hidden from in plain sight 2"
+end
+
+get '/cat' do
+  "<div>
+    <img src='http://bit.ly/1eze8aE'>
+   </div>"
 end
